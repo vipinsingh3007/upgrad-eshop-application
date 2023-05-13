d2f2bd0b441fc9aca55dd4fba42de800e4fe53f1
Vipinkumar Singh <vipinsingh3007@gmail.com>
2023-05-14 04:29:33 +0530

    Adding App Bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from './components/header/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">Upgrad Eshop Application</header>
     </div>
   );
